e96c8f78043becf4de60da4d4e7bf508ffa76dcb
Jose Tomas <josetomascodecido@gmail.com>
2018-11-08 19:48:31 -0300

    working shits

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,15 @@ class OrdersController < ApplicationController
   def create
     @store = Store.find(params[:store_id])
     @product = Product.find(params[:product_id])
-    @order = Order.new(product_id: @product.id, user: current_user, price: @product.price)
-    if @order.save
+    @cart = current_user.cart
+    if @cart.pluck(:product_id).include?(params[:product_id].to_i)
+      #tengo que buscar dentrodel cart
+    else
+      current_user.orders.build(product_id: @product.id, price: @product.price)
+      # @order = Order.new(product_id: @product.id, user: current_user, price: @product.price)
+    end
+
+    if current_user.save
       redirect_to store_product_orders_path(@store, @product), notice: 'Tu producto fue agregado al carro!'
     else
       redirect_to store_path(@store), alert: 'Intenta nuevamente CTM!'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_022729) do
+ActiveRecord::Schema.define(version: 2018_11_07_005224) do
 
   create_table "billings", force: :cascade do |t|
     t.integer "order_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_022729) do
     t.datetime "updated_at", null: false
     t.boolean "payed", default: false
     t.decimal "price"
+    t.integer "quantity"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
