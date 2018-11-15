73d37fd52cf16ec7fdc520096d6999316a745325
Jose Tomas <josetomascodecido@gmail.com>
2018-11-15 20:23:45 -0300

    fixed orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,12 +9,15 @@ class OrdersController < ApplicationController
     @store = Store.find(params[:store_id])
     @product = Product.find(params[:product_id])
     @cart = current_user.cart
+
     if @cart.pluck(:product_id).include?(params[:product_id].to_i)
+      product = current_user.orders.find_by(product_id: params[:product_id])
+      product.quantity ? product.quantity += 1 : product.quantity = 1
     else
-      current_user.orders.build(product_id: @product.id, price: @product.price)
+      product = current_user.orders.build(product_id: @product.id, price: @product.price, payed: false)
       # @order = Order.new(product_id: @product.id, user: current_user, price: @product.price)
     end
-    if current_user.save
+    if product.save
       redirect_to store_product_orders_path(@store, @product), notice: 'Tu producto fue agregado al carro!'
     else
       redirect_to store_path(@store), alert: 'Intenta nuevamente CTM!'
